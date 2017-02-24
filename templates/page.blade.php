d5107839e4de717c5975446c08c021937265873d
cmho <carlymho@gmail.com>
2017-02-23 20:01:24 -0600

    switch to using featured images for page headers, a bunch more style changes, add CC license

--- templates/page.blade.php
+++ templates/page.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    <section class="lead">
+    @php
+      $get_thumb_from = get_post();
+      if(!has_post_thumbnail() && (wp_get_post_parent_id($get_thumb_from->ID) !== 0)) {
+        $get_thumb_from = get_post(wp_get_post_parent_id($get_thumb_from));
+      }
+    @endphp
+    <section class="lead"<?php if (has_post_thumbnail($get_thumb_from)) : ?> style="background-image: url('<?= wp_get_attachment_image_src(get_post_thumbnail_id($get_thumb_from->ID), 'full')[0]; ?>')"<?php endif; ?>>
       @include('partials.page-header')
     </section>
     <section class="main">
--- templates/partials/footer.blade.php
+++ templates/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="content-info">
   <div class="container">
-    @php(dynamic_sidebar('sidebar-footer'))
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" /></a>
   </div>
 </footer>
