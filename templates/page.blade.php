ac17168b5f6bd161ed842c07bf67b280e91ca352
cmho <carlymho@gmail.com>
2017-02-26 12:53:00 -0600

    use banner size instead of full size image on pages

--- templates/page.blade.php
+++ templates/page.blade.php
@@ -8,7 +8,7 @@
         $get_thumb_from = get_post(wp_get_post_parent_id($get_thumb_from));
       }
     @endphp
-    <section class="lead"<?php if (has_post_thumbnail($get_thumb_from)) : ?> style="background-image: url('<?= wp_get_attachment_image_src(get_post_thumbnail_id($get_thumb_from->ID), 'full')[0]; ?>')"<?php endif; ?>>
+    <section class="lead"<?php if (has_post_thumbnail($get_thumb_from)) : ?> style="background-image: url('<?= wp_get_attachment_image_src(get_post_thumbnail_id($get_thumb_from->ID), 'banner')[0]; ?>')"<?php endif; ?>>
       @include('partials.page-header')
     </section>
     <section class="main">
