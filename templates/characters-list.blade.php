c2d5c599c2d1050e587f8e4ce2053e28acfc73d2
cmho <carlymho@gmail.com>
2017-03-26 20:11:51 -0500

    Update API pages to use fancy headers

--- templates/characters-list.blade.php
+++ templates/characters-list.blade.php
@@ -6,6 +6,12 @@
 
 @section('content')
   @while(have_posts()) @php(the_post())
+    @php
+      $get_thumb_from = get_post();
+      if(!has_post_thumbnail() && (wp_get_post_parent_id($get_thumb_from->ID) !== 0)) {
+        $get_thumb_from = get_post(wp_get_post_parent_id($get_thumb_from));
+      }
+    @endphp
     <section class="lead"<?php if (has_post_thumbnail($get_thumb_from)) : ?> style="background-image: url('<?= wp_get_attachment_image_src(get_post_thumbnail_id($get_thumb_from->ID), 'banner')[0]; ?>')"<?php endif; ?>>
       @include('partials.page-header')
     </section>
